5951df6ad3983b04b40ec668bd67058d67921bcd
javierGuerraDevelop <133339273+javierGuerraDevelop@users.noreply.github.com>
2024-03-23 22:57:38 -0400

    creating repo

---
+++ src/README.md
@@ -0,0 +1,20 @@
+# E-Commerce Backend
+
+## Description
+
+This project is the backend for an e-commerce site, built using Node.js, Express.js, and Sequelize. It interfaces with a MySQL database to manage categories, products, and tags for an e-commerce business.
+
+## Technologies Used
+
+-   Node.js
+-   Express.js
+-   Sequelize ORM
+-   MySQL
+
+## Installation
+
+To get started, clone this repository to your local machine, then run the following commands.
+
+1. $ npm install
+2. $ npm run seed
+3. $ npm start
---
+++ src/routes/api/product-routes.js
@@ -0,0 +1,105 @@
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    if (req.body.tagIds && req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+      await ProductTag.bulkCreate(productTagIdArr);
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    const productTags = await ProductTag.findAll({
+      where: { product_id: req.params.id },
+    });
+
+    const productTagIds = productTags.map(({ tag_id }) => tag_id);
+    const newProductTags = req.body.tagIds
+      .filter((tag_id) => !productTagIds.includes(tag_id))
+      .map((tag_id) => {
+        return {
+          product_id: req.params.id,
+          tag_id,
+        };
+      });
+    const productTagsToRemove = productTags.filter(({ tag_id }) => !req.body.tagIds.includes(tag_id)).map(({ id }) => id);
+
+    await Promise.all([ProductTag.destroy({ where: { id: productTagsToRemove } }), ProductTag.bulkCreate(newProductTags)]);
+
+    const updatedProduct = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const productDelete = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productDelete) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Product successfully deleted!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
